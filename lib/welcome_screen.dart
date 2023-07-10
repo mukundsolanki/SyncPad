bdcb5a448aca06bd012344a91adab3137e06c3fb
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-11 02:29:11 +0530

    Updated the home screen design

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -11,37 +11,129 @@ class WelcomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Please enter your computer\'s IP address:',
-            style: TextStyle(fontSize: 20),
+        backgroundColor: Colors.blue,
+        elevation: 0,
+        title: const Center(
+            child: Text(
+          'SyncPad',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white, // Set the text color
           ),
-          SizedBox(height: 16),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: TextField(
-              onChanged: (value) {
-                ipAddress = value;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'IP Address',
+        )),
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            ClipPath(
+              clipper: BackgroundClipper(),
+              child: Container(
+                color: Colors.blue,
               ),
             ),
-          ),
-          SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {
-              onIPSubmit(ipAddress);
-            },
-            child: Text('Submit'),
-          ),
-        ],
+            Center(
+              child: SingleChildScrollView(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Welcome to SyncPad',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: TextField(
+                          onChanged: (value) {
+                            ipAddress = value;
+                          },
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your IP Address',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          onIPSubmit(ipAddress);
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.blue),
+                        ),
+                        child: const Text('Connect'),
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        'Instructions:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      _buildBulletPoint('Step 1: Download SyncPad for Windows'),
+                      _buildBulletPoint(
+                          'Step 2: Find your computer\'s IP address'),
+                      _buildBulletPoint('Step 3: Enter the IP address'),
+                      const SizedBox(height: 8),
+                      const Text(
+                        'Note: Make sure both devices are connected on the same network',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Widget _buildBulletPoint(String text) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(width: 10),
+        const Icon(Icons.brightness_1, size: 8),
+        const SizedBox(width: 10),
+        Expanded(child: Text(text)),
+      ],
+    );
+  }
+}
+
+class BackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height * 0.75);
+    path.quadraticBezierTo(
+      size.width * 0.5,
+      size.height,
+      size.width,
+      size.height * 0.75,
+    );
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
