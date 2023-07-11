f1bc3ea9364e603ea2428ae306baee9d74510ea2
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-11 12:47:45 +0530

    Updated the design of the Welcome Screen

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   final Function(String) onIPSubmit;
 
-  WelcomeScreen({required this.onIPSubmit});
+  const WelcomeScreen({super.key, required this.onIPSubmit});
 
   @override
   Widget build(BuildContext context) {
@@ -11,88 +11,120 @@ class WelcomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.black,
         elevation: 0,
         title: const Center(
-            child: Text(
-          'SyncPad',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white, // Set the text color
+          child: Text(
+            'SyncPad',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 65, 227, 168),
+            ),
           ),
-        )),
+        ),
       ),
       body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.black, Colors.grey[900]!],
+          ),
+        ),
         child: Stack(
+          alignment: Alignment.center,
           children: [
-            ClipPath(
-              clipper: BackgroundClipper(),
-              child: Container(
-                color: Colors.blue,
-              ),
-            ),
             Center(
               child: SingleChildScrollView(
                 child: Container(
                   margin: const EdgeInsets.symmetric(horizontal: 20),
                   padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
                   ),
                   child: Column(
                     children: [
-                      const Text(
-                        'Welcome to SyncPad',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      const Text.rich(
+                        TextSpan(
+                          text: 'Welcome to ',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: 'SyncPad',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 65, 227, 168),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                       const SizedBox(height: 16),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 16),
                         child: TextField(
+                          keyboardType: TextInputType.number,
                           onChanged: (value) {
                             ipAddress = value;
                           },
                           decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                              ),
+                            ),
                             labelText: 'Enter your IP Address',
+                            labelStyle: TextStyle(color: Colors.white),
+                          ),
+                          style: const TextStyle(
+                            color: Colors.white,
                           ),
                         ),
                       ),
                       const SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: () {
-                          onIPSubmit(ipAddress);
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 30,
+                        child: IconButton(
+                          onPressed: () {
+                            onIPSubmit(ipAddress);
+                          },
+                          icon: const Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          ),
                         ),
-                        child: const Text('Connect'),
                       ),
-                      const SizedBox(height: 16),
+                      const SizedBox(height: 32),
                       const Text(
-                        'Instructions:',
+                        '© Mukund Solanki',
                         style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 14,
                           fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                      ),
-                      const SizedBox(height: 8),
-                      _buildBulletPoint('Step 1: Download SyncPad for Windows'),
-                      _buildBulletPoint(
-                          'Step 2: Find your computer\'s IP address'),
-                      _buildBulletPoint('Step 3: Enter the IP address'),
-                      const SizedBox(height: 8),
-                      const Text(
-                        'Note: Make sure both devices are connected on the same network',
                       ),
                     ],
                   ),
@@ -104,36 +136,4 @@ class WelcomeScreen extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildBulletPoint(String text) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(width: 10),
-        const Icon(Icons.brightness_1, size: 8),
-        const SizedBox(width: 10),
-        Expanded(child: Text(text)),
-      ],
-    );
-  }
-}
-
-class BackgroundClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0, size.height * 0.75);
-    path.quadraticBezierTo(
-      size.width * 0.5,
-      size.height,
-      size.width,
-      size.height * 0.75,
-    );
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
